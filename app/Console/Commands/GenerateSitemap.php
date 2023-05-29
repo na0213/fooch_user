5e58b7e5e8f0e117c0bef1291acd0da5b3aa2eb1
na0213 <natsumikan0213@gmail.com>
2023-05-29 17:19:39 +0900

    site_map

--- app/Console/Commands/GenerateSitemap.php
+++ app/Console/Commands/GenerateSitemap.php
@@ -19,46 +19,46 @@ class GenerateSitemap extends Command
         $sitemap = Sitemap::create();
 
         // Topページ
-        $sitemap->add(Url::create(route('welcome'))
+        $sitemap->add(Url::create(route('welcome', [], true))
             ->setLastModificationDate(now())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_ALWAYS)
             ->setPriority(1.0));
 
         // 商品一覧
-        $sitemap->add(Url::create(route('top.index'))
+        $sitemap->add(Url::create(route('top.index', [], true))
             ->setLastModificationDate(now())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
             ->setPriority(0.8));
 
         // 商品詳細
         Product::all()->each(function ($product) use ($sitemap) {
-            $sitemap->add(Url::create(route('top.show', $product))
+            $sitemap->add(Url::create(route('top.show', $product, true))
                 ->setLastModificationDate($product->updated_at)
                 ->setChangeFrequency(Url::CHANGE_FREQUENCY_WEEKLY)
                 ->setPriority(0.5));
         });
 
-        $sitemap->add(Url::create(route('top.store'))
+        $sitemap->add(Url::create(route('top.store', [], true))
             ->setLastModificationDate(now())
             ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
             ->setPriority(0.5));
 
-        $sitemap->add(Url::create(route('top.whatis'))
+        $sitemap->add(Url::create(route('top.whatis', [], true))
         ->setLastModificationDate(now())
         ->setChangeFrequency(Url::CHANGE_FREQUENCY_ALWAYS)
         ->setPriority(1.0));
 
-        $sitemap->add(Url::create(route('top.owner_contact'))
+        $sitemap->add(Url::create(route('top.owner_contact', [], true))
         ->setLastModificationDate(now())
         ->setChangeFrequency(Url::CHANGE_FREQUENCY_ALWAYS)
         ->setPriority(1.0));
 
-        $sitemap->add(Url::create(route('terms'))
+        $sitemap->add(Url::create(route('terms', [], true))
         ->setLastModificationDate(now())
         ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
         ->setPriority(0.5));
 
-        $sitemap->add(Url::create(route('legal'))
+        $sitemap->add(Url::create(route('legal', [], true))
         ->setLastModificationDate(now())
         ->setChangeFrequency(Url::CHANGE_FREQUENCY_MONTHLY)
         ->setPriority(0.5));
